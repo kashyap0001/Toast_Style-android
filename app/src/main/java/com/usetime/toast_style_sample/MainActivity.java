273734fefd53729b76f75e3667a90b913e919874
kashyap_99 <rathodkashyap100@gmail.com>
2020-03-17 14:30:34 +0530

    Second Commit

--- app/src/main/java/com/usetime/toast_style_sample/MainActivity.java
+++ app/src/main/java/com/usetime/toast_style_sample/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.usetime.lib_toaststyle.Toast_style;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast_style style = new Toast_style(MainActivity.this);
+        style.ShowSimpleToast("Example toast");
+
+        style.DarkToast("Dark Toast");
     }
 }
